810272c07ceca6fe5e16264d802e1a66fed63ed0
Jalel Yakdi <jalelyakdi@MacBook-Pro.local>
2020-10-19 19:03:29 +0200

    :rocket: initialize server before deploying it

---
+++ src/server/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
+        channel: "jalelyak",
+        description: "Epic MERN stack TIK TOK",
+        song: "99 problems but REACT aint one",
+        likes: "111",
+        messages: "222",
+        shares: "333"
+    },
+    {
+        url: "http://pedagogie.ac-limoges.fr/test/IMG/mp4/vts_01.mp4",
+        channel: "jalelyak",
+        description: "Epic MERN stack TIK TOK",
+        song: "99 problems but REACT aint one",
+        likes: "1111",
+        messages: "2222",
+        shares: "3333"
+    }
+]
+
+export default data;
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,58 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 
+import Data from './data.js';
+import Videos from './dbModel.js';
+
+// app config
+const app = express();
+const port = 9000;
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeaders('Access-Control-Allow-Origin', '*'),
+    res.setHeaders('Access-Control-Allow-Headers', '*'),
+    next();
+})
+
+//DB config
+const connection_url = 'mongodb+srv://admin:KGPFhyiMspXkQUCI@cluster0.iuitv.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+// api endpoints
+app.get('/', (req, res) => res.status(200).send('Hello world'));
+
+app.get('/v1/posts', (req, res) => res.status(200).send(Data));
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.post('/v2/posts', (req, res) => {
+    // POST request if to ADD data to the database
+    // It will let us ADD a video DOCUMENT to the videos COLLECTION
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+})
+
+// listeners
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
